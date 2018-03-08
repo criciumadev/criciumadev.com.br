27091242c88b02f7041d63dfe8b136ea15cb301a
lucasferreira <panchorf@gmail.com>
2018-03-08 11:48:15 -0300

    README, novo setup de vagas, otimização de imagens e javascript

--- gulpfile.js
+++ gulpfile.js
@@ -1,113 +1,182 @@
-var gulp        = require('gulp'),
-    jshint      = require('gulp-jshint'),
-    babel       = require('gulp-babel'),
-    watch       = require('gulp-watch'),
-    uglify      = require('gulp-uglify'),
-    htmlmin     = require('gulp-htmlmin'),
-    runSequence = require('run-sequence'),
-    concat      = require('gulp-concat'),
-    fileinclude = require('gulp-file-include'),
-    sass        = require('gulp-sass'),
-    del         = require('del'),
-    cssmin      = require('gulp-clean-css'),
-    bulkSass    = require('gulp-sass-bulk-import'),
-    webserver   = require('gulp-webserver');
+var gulp = require("gulp"),
+  jshint = require("gulp-jshint"),
+  babel = require("gulp-babel"),
+  watch = require("gulp-watch"),
+  uglify = require("gulp-uglify"),
+  htmlmin = require("gulp-htmlmin"),
+  runSequence = require("run-sequence"),
+  concat = require("gulp-concat"),
+  fileinclude = require("gulp-file-include"),
+  sass = require("gulp-sass"),
+  del = require("del"),
+  cssmin = require("gulp-clean-css"),
+  bulkSass = require("gulp-sass-bulk-import"),
+  webserver = require("gulp-webserver");
 
 var PATH = {
-  dest:      './www',
-  src:       './src',
-  data:      './src/assets/data',
-  css:       './src/assets/stylesheets',
-  js:        './src/assets/javascripts',
-  templates: './src/templates',
-  node_modules: './node_modules'
+  dest: "./www",
+  src: "./src",
+  data: "./src/assets/data",
+  css: "./src/assets/stylesheets",
+  js: "./src/assets/javascripts",
+  templates: "./src/templates",
+  node_modules: "./node_modules",
+};
+
+function requireUncached(module) {
+  delete require.cache[require.resolve(module)];
+  return require(module);
 }
 
-gulp.task('sass-minify', function () {
-    return gulp.src([
-      PATH.css + '/index.scss'
-    ])
+function getRandomColor() {
+  var letters = "0123456789ABCDEF";
+  var color = "#";
+
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+
+  return color;
+}
+
+function appendCompanyAlias(job) {
+  var nameSplited = job.company_name.split(" ");
+  var firstLetter = nameSplited[0][0];
+  var lastLetter = nameSplited[1] ? nameSplited[1][0] : nameSplited[0][1];
+
+  job.alias = (firstLetter + lastLetter).toUpperCase();
+
+  return job;
+}
+
+gulp.task("sass-minify", function() {
+  return gulp
+    .src([PATH.css + "/index.scss"])
     .pipe(bulkSass())
-    .pipe(sass().on('error', sass.logError))
+    .pipe(sass().on("error", sass.logError))
     .pipe(cssmin())
-    .pipe(concat('all.min.css'))
-    .pipe(gulp.dest(PATH.dest + '/assets'));
+    .pipe(concat("all.min.css"))
+    .pipe(gulp.dest(PATH.dest + "/assets"));
 });
 
-gulp.task('js-minify', function () {
-  return gulp.src([
-    PATH.node_modules + '/handlebars/dist/handlebars.min.js',
-    PATH.js + '/slick.js',
-    PATH.js + '/jquery.validate.js',
-    PATH.js + '/moment.min.js',
-    PATH.js + '/widgets.js',
-    PATH.js + '/custom.js',
-  ])
-  .pipe(concat('all.min.js'))
-  .pipe(uglify())
-  .pipe(gulp.dest(PATH.dest + '/assets'));
+gulp.task("js-minify", function() {
+  return gulp
+    .src([
+      PATH.node_modules + "/handlebars/dist/handlebars.min.js",
+      PATH.js + "/slick.js",
+      PATH.js + "/jquery.validate.js",
+      PATH.js + "/moment.min.js",
+      PATH.js + "/widgets.js",
+      PATH.js + "/custom.js",
+    ])
+    .pipe(concat("all.min.js"))
+    .pipe(uglify())
+    .pipe(gulp.dest(PATH.dest + "/assets"));
 });
 
-gulp.task('clean-html', function() {
-  del(PATH.dest + '/*.html');
+gulp.task("clean-html", function() {
+  del(PATH.dest + "/*.html");
 });
 
-gulp.task('copy-data', function() {
-  gulp.src(PATH.data + '/*').pipe(gulp.dest(PATH.dest + '/assets/data'));
+gulp.task("copy-data", function() {
+  gulp.src(PATH.data + "/*").pipe(gulp.dest(PATH.dest + "/assets/data"));
 });
 
-
-gulp.task('copy-images', function() {
-  gulp.src(PATH.src + '/assets/icons/*').pipe(gulp.dest(PATH.dest + '/assets/icons'));
-  gulp.src(PATH.src + '/assets/images/*').pipe(gulp.dest(PATH.dest + '/assets/images'));
-  gulp.src(PATH.src + '/assets/images/**/').pipe(gulp.dest(PATH.dest + '/assets/images'));
-  gulp.src(PATH.src + '/assets/images/**/*').pipe(gulp.dest(PATH.dest + '/assets/images'));
+gulp.task("copy-images", function() {
+  gulp.src(PATH.src + "/assets/icons/*").pipe(gulp.dest(PATH.dest + "/assets/icons"));
+  gulp.src(PATH.src + "/assets/images/*").pipe(gulp.dest(PATH.dest + "/assets/images"));
+  gulp.src(PATH.src + "/assets/images/**/").pipe(gulp.dest(PATH.dest + "/assets/images"));
+  gulp.src(PATH.src + "/assets/images/**/*").pipe(gulp.dest(PATH.dest + "/assets/images"));
 });
 
-gulp.task('includes-html', function() {
-  gulp.src(PATH.templates + '/*.html').pipe(fileinclude({
-    prefix: '@@'
-  })).pipe(gulp.dest(PATH.dest));
-  // gulp.src(PATH.templates + '/.htaccess').pipe(gulp.dest(PATH.dest));
+gulp.task("includes-html", function() {
+  gulp
+    .src(PATH.templates + "/*.html")
+    .pipe(
+      (function() {
+        requireUncached(PATH.data + "/jobs.json");
+
+        var colors = {};
+        var vagas = require(PATH.data + "/jobs.json").map(function(job) {
+          job = appendCompanyAlias(job);
+
+          if (!job.color) {
+            if (colors[job.company_name]) {
+              job.color = colors[job.company_name];
+            } else {
+              job.color = getRandomColor();
+              colors[job.company_name] = job.color;
+            }
+          }
+
+          return job;
+        });
+
+        return fileinclude({
+          prefix: "@@",
+          context: {
+            vagas: {
+              featured: JSON.stringify(
+                vagas.filter(function(job) {
+                  return !!job.featured;
+                })
+              ),
+              jobs: JSON.stringify(
+                vagas.filter(function(job) {
+                  return !job.featured;
+                })
+              ),
+            },
+          },
+        });
+      })()
+    )
+    .pipe(gulp.dest(PATH.dest));
 });
 
-gulp.task('minify-html', function () {
+gulp.task("minify-html", function() {
   // Timeout
-  setTimeout(function () {
-    gulp.src(PATH.dest + '/*.html').pipe(htmlmin({
-      collapseWhitespace: true
-    })).pipe(gulp.dest(PATH.dest));
+  setTimeout(function() {
+    gulp
+      .src(PATH.dest + "/*.html")
+      .pipe(
+        htmlmin({
+          collapseWhitespace: true,
+        })
+      )
+      .pipe(gulp.dest(PATH.dest));
   }, 2000);
 });
 
-gulp.task('generate-html', function() {
-  runSequence(['clean-html'], ['includes-html'], ['minify-html']);
+gulp.task("generate-html", function() {
+  runSequence(["clean-html"], ["includes-html"], ["minify-html"]);
 });
 
-gulp.task('watchs', function () {
-  gulp.watch(PATH.css + '/**/*.scss', ['sass-minify']);
-  gulp.watch(PATH.js + '/**/*.js', ['js-minify']);
-  gulp.watch([PATH.templates + '/*.html', PATH.templates + '/**/*.html'], ['generate-html']);
-  gulp.watch([PATH.templates + '/assets/icons/*', PATH.templates + '/assets/images/*'], ['copy-images']);
-  gulp.watch(PATH.data + '/*', ['copy-data']);
+gulp.task("watchs", function() {
+  gulp.watch(PATH.css + "/**/*.scss", ["sass-minify"]);
+  gulp.watch(PATH.js + "/**/*.js", ["js-minify"]);
+  gulp.watch([PATH.templates + "/*.html", PATH.templates + "/**/*.html", PATH.data + "/*.json"], ["generate-html"]);
+  gulp.watch([PATH.templates + "/assets/icons/*", PATH.templates + "/assets/images/*"], ["copy-images"]);
+  gulp.watch(PATH.data + "/*", ["copy-data"]);
 });
 
-gulp.task('default', function () {
+gulp.task("default", function() {
   return runSequence(
-    ['sass-minify'],
-    ['copy-images'],
-    ['js-minify'],
-    ['generate-html'],
-    ['copy-data'],
-    ['watchs'],
-    ['webserver']
+    ["sass-minify"],
+    ["copy-images"],
+    ["js-minify"],
+    ["generate-html"],
+    ["copy-data"],
+    ["watchs"],
+    ["webserver"]
   );
 });
 
-gulp.task('webserver', function() {
-  gulp.src(PATH.dest)
-    .pipe(webserver({
+gulp.task("webserver", function() {
+  gulp.src(PATH.dest).pipe(
+    webserver({
       livereload: true,
-      port: 8000
-    }));
+      port: 8000,
+    })
+  );
 });
--- src/assets/javascripts/custom.js
+++ src/assets/javascripts/custom.js
@@ -4,19 +4,49 @@ $(document).ready(function() {
 	SITE.init();
 });
 
-$(window).resize(function() {
-});
+$(window).resize(function() {});
+
+function friendlyDate(date) {
+	var _date = moment(date);
+	var currentDate = moment();
+	var isSameYear = _date.year() == currentDate.year();
+	var isSameMonth = _date.month() == currentDate.month();
+
+	if (currentDate.date() == _date.date() && isSameYear && isSameMonth) {
+		return "HOJE";
+	}
+
+	if (currentDate.date() - 1 == _date.date()) {
+		return "ONTEM";
+	}
+
+	return "HÁ " + _date.date() + " " + (_date.date() > 1 ? "DIAS" : "DIA");
+}
 
 SITE.init = function() {
 	this.sliderEvents();
 	this.goToTop();
 	this.menuActive();
 	this.newsletter();
+
+	/* HOME */
+	if ($(document.body).hasClass("page-home")) {
+		Widgets.eventsFeed.fetch();
+		Widgets.postsFeed.fetch();
+		Widgets.videosFeed.fetch();
+	}
+
+	/* VAGAS */
+	if ($(document.body).hasClass("page-vagas")) {
+		$(".page-vagas .list-jobs li").each(function(i) {
+			var spanDate = $("span[data-date]", this);
+			spanDate.text(friendlyDate(spanDate.data("date")));
+		});
+	}
 };
 
 SITE.sliderEvents = function() {
-
-	var $scope = $('*[data-scope="events-feed"]:first').find('.list-cards');
+	var $scope = $('*[data-scope="events-feed"]:first').find(".list-cards");
 	var $scopeArrows = $('*[data-scope="events-feed-arrows"]:first');
 
 	$scope.slick({
@@ -31,87 +61,85 @@ SITE.sliderEvents = function() {
 		prevArrow: $('*[data-arrow="slider-events-prev"]:first', $scopeArrows),
 		nextArrow: $('*[data-arrow="slider-events-next"]:first', $scopeArrows),
 		responsive: [
-		{
-			breakpoint: 1200,
-			settings: {
-				slidesToShow: 2
-			}
-		},
-		{
-			breakpoint: 992,
-			settings: {
-				slidesToShow: 1
-			}
-		},
-		]
+			{
+				breakpoint: 1200,
+				settings: {
+					slidesToShow: 2,
+				},
+			},
+			{
+				breakpoint: 992,
+				settings: {
+					slidesToShow: 1,
+				},
+			},
+		],
 	});
-
 };
 
 SITE.goToTop = function() {
-	$('*[data-scope="go-to-top"]:first').on('click', function(e) {
+	$('*[data-scope="go-to-top"]:first').on("click", function(e) {
 		e.preventDefault();
-		$('body,html').animate({ scrollTop : 0 }, 500);
+		$("body,html").animate({ scrollTop: 0 }, 500);
 	});
-}
+};
 
 SITE.menuActive = function() {
 	// var $scope = $('*[data-scope=header]:first');
 	// $('a[href^="' + location.pathname + '"]', $scope).addClass('active');
-}
+};
 
 SITE.newsletter = function() {
-	var $form    = $('*[data-scope=newsletter]:first'),
-		$input   = $('*[data-item=input]:first', $form),
-		$button  = $('*[data-item=button]:first', $form),
-		$message = $('*[data-item=subscribe-result]:first', $form);
+	var $form = $("*[data-scope=newsletter]:first"),
+		$input = $("*[data-item=input]:first", $form),
+		$button = $("*[data-item=button]:first", $form),
+		$message = $("*[data-item=subscribe-result]:first", $form);
 
 	$form.validate({
 		rules: {
 			EMAIL: {
 				required: true,
-				email: true
+				email: true,
 			},
 		},
 		messages: {
 			EMAIL: {
-				required: 'Email é obrigatório',
-				email: 'Insira um email válido'
-			}
-		}
+				required: "Email é obrigatório",
+				email: "Insira um email válido",
+			},
+		},
 	});
 
-	$form.on('submit', function(e) {
+	$form.on("submit", function(e) {
 		e.preventDefault();
 
 		$message.hide();
 
-		if($form.valid()){
+		if ($form.valid()) {
 			$button.val("Enviando...");
 
 			$.ajax({
-				type: $form.attr('method'),
-				url: $form.attr('action'),
+				type: $form.attr("method"),
+				url: $form.attr("action"),
 				data: $form.serialize(),
 				cache: false,
-				dataType: 'json',
-				contentType: 'application/json; charset=utf-8',
-				error: function (err) {
+				dataType: "json",
+				contentType: "application/json; charset=utf-8",
+				error: function(err) {
 					$button.val("Assinar");
-					$message.text('Ocorreu um erro. Tente novamente.').show();
+					$message.text("Ocorreu um erro. Tente novamente.").show();
 				},
-				success: function (data) {
+				success: function(data) {
 					$button.val("Assinar");
-					if (data.result === 'success') {
-						$message.text('Cadastrado com sucesso!').show();
+					if (data.result === "success") {
+						$message.text("Cadastrado com sucesso!").show();
 					} else {
-						$message.text('Ocorreu um erro ou o email já está cadastrado.').show();
+						$message.text("Ocorreu um erro ou o email já está cadastrado.").show();
 					}
-				}
+				},
 			});
 
 			return;
 		}
 	});
-	
-}
+};
